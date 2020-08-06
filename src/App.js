d819d6b40eb77204ac7a01f7e9d3647e545b0fbc
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-08-06 10:45:51 +0900

    processing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     if (data.hits.length < 1) {
